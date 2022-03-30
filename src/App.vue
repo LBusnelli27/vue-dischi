7f239ef8a46f39ef7f3789ed71f8c8e3b2d8875d
Busna27 <lucabusnelli2001@gmail.com>
2022-03-30 15:37:31 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import "./assets/scss/style.scss";
+  
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    
+    <nav class="navbar navbar-light">
+      <div class="container-fluid">
+        <img src="https://cdn-icons-png.flaticon.com/512/174/174872.png" alt="Spotify Logo">
+      </div>
+    </nav>
   </div>
 </template>
 
@@ -11,5 +15,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import "../assets/scss/style.scss";
+  div {
+    nav{
+      background-color: $lightBlueGray;
+      img {
+        height: 60px;
+      }
+    }
+  }
 </style>
